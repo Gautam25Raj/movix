deebe2b1107ae1cde4d0f103f577b411e1bba4fe
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-05-10 17:55:11 +0530

    Base commit
    Update index.html
    
    Add logo and Error photos
    
    Add pages folder
    
    Add all routes for Project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import HeroBanner from './heroBanner/HeroBanner';
+
+const Home = () => {
+  return (
+    <div>
+      <HeroBanner />
+    </div>
+  );
+};
+export default Home;
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,4 @@
+const HomeBanner = () => {
+  return <div className="heroBanner">HeroBanner</div>;
+};
+export default HomeBanner;
